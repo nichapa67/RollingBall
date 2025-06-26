22cd4f2a901cfadd36740d6f6e98c323801c8905
nichapa67 <nichapa.du@ku.th>
2025-06-26 17:19:44 +0700

    Add files via upload

--- Lib/RolligBall.java
+++ Lib/RolligBall.java
@@ -3,11 +3,27 @@ import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
 
-public class RolligBall extends JPanel{
+public class RolligBall extends JPanel implements ActionListener{
+    int x =100;
+    int y =90;
+    int start_angel =0;
+    public RolligBall(){
+        Timer t = new Timer(100, this);
+        t.start();
+    }
+
     public void paintComponent(Graphics g){
         super.paintComponent(g);
-        g.drawOval(100, 90, 80, 80);
-        g.fillArc(100, 90, 80, 80, 0, 0);
+        g.drawOval(x, y, 80, 80);
+        g.fillArc(x, y, 80, 80, start_angel, 180);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        x=x-2;
+        start_angel=start_angel+2;
+        if(start_angel==360) start_angel=0;
+        repaint();
     }
     
 }
